a27e8cc75869ec48c1a229cd7d5d003f45e45434
hung phan <hung.phan@episerver.com>
2018-08-31 17:02:16 +0700

    init repo

---
+++ webpack-demo/src/Dialog.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class Dialog extends Components{
+    render(){
+        return(
+            <div>
+                Test
+            </div>
+        )
+    }
+}
---
+++ webpack-demo/src/Sum.js
@@ -0,0 +1,10 @@
+var sum = function(a, b){
+    return a +b;
+}
+
+// const multiple = function(a, b){
+//     return a * b;
+// }
+
+// export multiple;
+export default sum;
